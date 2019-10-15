952d36dfe7dcf59f95f2914f67bbee45de02d8a2
IVESC1 <Maxiscade1@gmail.com>
2019-10-15 16:49:49 +1300

    added game methods

--- src/main/java/nz/ac/eit/Player.java
+++ src/main/java/nz/ac/eit/Player.java
@@ -6,4 +6,12 @@ public class Player {
     public Player(int score){
         this.score = score;
     }
+
+    public int getScore(){
+        return this.score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
 }
